405f55f651ce47a9237e87ac8084e56808594dc9
kwakwoohyun010@gmail.com <kwakwoohyun010@gmail.com>
2020-11-26 19:03:35 +0900

    feat: Add third day backjoon algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -5,29 +6,14 @@ public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         Scanner sc = new Scanner(System.in);
-        int repeatCount = sc.nextInt();
-        String example[] = new String[repeatCount];
-        for (int i = 0; i < repeatCount; i++) {
-            example[i] = sc.next();
-        }
 
-        for (int i = 0; i < repeatCount; i++) {
-            int num[] = new int[example[i].length()];
-            int sum = 0;
-            char a[] = example[i].toCharArray();
-            int result =0;
-            for (int j = 0; j < a.length; j++) {
-                if (a[j] == 'O') {
-                    num[j] = sum + 1;
-                    sum++;
-                } else {
-                    sum = 0;
-                }
-                if (num[j]!=0){
-                    result=result+num[j];
-                }
-            }
-            System.out.println(result);
+    }
+
+    public long sum(int[] a){
+        int sum = 0;
+        for (int i=0; i<a.length;i++){
+            sum = sum + a[i];
         }
+        return sum;
     }
 }
