36a5f8a2069faa00387f89cf6eeabf81be70f230
kwakwoohyun010@gmail.com <kwakwoohyun010@gmail.com>
2020-12-02 18:51:56 +0900

    feat: Add 2675

--- src/baekjoonString/p2675.java
+++ src/baekjoonString/p2675.java
@@ -1,12 +1,14 @@
+package baekjoonString;
+
 import java.util.Scanner;
 
-public class Main {
+public class p2675 {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         Scanner sc = new Scanner(System.in);
         int totalCount = sc.nextInt();
-        for (int i=0;i<totalCount;i++){
+        for (int i = 0; i < totalCount; i++) {
             int repeatCount = sc.nextInt();
             char ch[] = sc.next().toCharArray();
             for (int j = 0; j < ch.length; j++) {
