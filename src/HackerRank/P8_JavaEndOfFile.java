7c7a901c1698004ed0d7da640cb1e0ef2b2cdf1c
kwakwoohyun010@gmail.com <kwakwoohyun010@gmail.com>
2022-01-29 21:59:56 +0900

    feat: Add HackerRank java end of file

---
+++ src/HackerRank/P8_JavaEndOfFile.java
@@ -0,0 +1,8 @@
+package HackerRank;
+
+/**
+ * The challenge here is to read n lines of input until you reach EOF, then number and print all n lines of content.
+ * Hint: Java's Scanner.hasNext() method is helpful for this problem.
+ */
+public class P8_JavaEndOfFile {
+}
